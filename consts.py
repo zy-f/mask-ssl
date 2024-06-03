73ddf0a274e8edd5fc38e5db0c1543fc40ffdb20
Ubuntu <ubuntu@shared-ec2-instance.fake>
2024-06-03 06:48:49 +0000

    commit output logs, res50 config

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 PROJ_DIR = '/home/ubuntu/mask-ssl'
 WEIGHT_DIR = f"{PROJ_DIR}/_weights"
-OUTPUT_DIR = f"{PROJ_DIR}/_outputs"
+OUTPUT_DIR = f"{PROJ_DIR}/outputs"
 CONFIG_DIR = f"{PROJ_DIR}/configs"
 LOG_DIR = f"{PROJ_DIR}/exp_logs"
 
