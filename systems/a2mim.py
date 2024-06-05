99097363250f267ed99043fb3cdaaaf8c1b7de0e
Ubuntu <ubuntu@ip-172-31-9-152.us-west-2.compute.internal>
2024-06-05 00:21:05 +0000

    modify weight loading logic

--- systems/a2mim.py
+++ systems/a2mim.py
@@ -13,9 +13,7 @@ WEIGHTS = {
 }
 
 def load_weights(model, weight_name='resnet50', delete_keys=[]):
-    if weight_name not in WEIGHTS.keys():
-        raise ValueError(f'invalid weight name {weight_name}')
-    elif weight_name == 'resnet50':
+    if weight_name == 'resnet50':
         raw_ckpt = torch.load(WEIGHTS[weight_name])
         sd = {k[k.find('.')+1:] : v for k,v in raw_ckpt['state_dict'].items()}
         for dk in delete_keys:
